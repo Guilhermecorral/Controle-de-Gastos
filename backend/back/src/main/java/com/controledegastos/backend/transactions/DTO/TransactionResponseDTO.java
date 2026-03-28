f8dde13964fea2c04a1997de4ab70e90aba8290c
Guilherme <guilhermecorral.01@gmail.com>
2026-03-28 18:51:02 -0300

    Commit Add TransactionResponseDTO

---
+++ backend/back/src/main/java/com/controledegastos/backend/transactions/DTO/TransactionResponseDTO.java
@@ -0,0 +1,22 @@
+package com.controledegastos.backend.transactions.DTO;
+
+import com.controledegastos.backend.transactions.Transaction.TransactionCategory;
+import com.controledegastos.backend.transactions.Transaction.TransactionType;
+import com.controledegastos.backend.transactions.Transaction.PaymentMethod;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+public record TransactionResponseDTO (
+
+   Long id,
+   TransactionType type,
+   String description,
+   TransactionCategory category,
+   BigDecimal amount,
+   PaymentMethod paymentMethod,
+   LocalDate TransactionDate,
+   LocalDateTime createdAt
+
+) {}
